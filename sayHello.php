03293f77b788af43095d7924692851953308da7b
Charles <codprestige10@gmail.com>
2016-02-29 18:31:27 -0500

    first php push

---
+++ sayHello.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link href="..css/phpMM.css" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body>
+		<div id="header"><h1>PHP & Mysql: the missing manual</h1></div>
+		<div id="example">Example 2-1</div>
+		<div id="content">
+			<h1>Hello <?php echo $_REQUEST['name']; ?></h1>
+
+			<p>Great to meet you</p>
+		</div>
+	</body>
+</html>
