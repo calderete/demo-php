c70dd802c046c292e26195a824455358c2d5f2ce
Charles <codprestige10@gmail.com>
2016-03-02 17:04:03 -0500

    put in facebook and twitter handle correct, as well as trim for whitespace

--- scripts/getFormInfo.php
+++ scripts/getFormInfo.php
@@ -1,13 +1,20 @@
 <?php 
-	$first_name = $_REQUEST["first_name"];
-	$last_name = $_REQUEST["last_name"];
-	$email = $_REQUEST["email"];
-	$facebook_url = $_REQUEST["facebook_url"];
+	$first_name = trim($_REQUEST["first_name"]);
+	$last_name = trim($_REQUEST["last_name"]);
+	$email = trim($_REQUEST["email"]);
+	$facebook_url = trim($_REQUEST["facebook_url"]);
 	$position = strpos($facebook_url, "facebook.com");
 	if ($position === false) {
 		$facebook_url = "http://www.facebook.com/" . $facebook_url;
 	}
-	$twitter_handle = $_REQUEST["twitter_handle"];
+	$twitter_handle = trim($_REQUEST["twitter_handle"]);
+	$twitter_url = "http://www.twitter.com/";
+	$twitter_handle_check = strpos($twitter_handle, "@");
+	if ($twitter_handle_check === false) {
+		$twitter_url = $twitter_url . $twitter_handle;
+	} else {
+		$twitter_url = $twitter_url . substr($twitter_handle, $twitter_handle_check + 1);
+	}
 
 ?>
 
@@ -25,8 +32,8 @@
 			<p>
 				Name: <?php echo $first_name . " " . $last_name; ?><br />
 				Email: <?php echo $email; ?><br />
-				<a href="<?php echo $facebook_url; ?>">Your Facebook</a><br />
-				Twitter: <?php echo $twitter_handle; ?><br />
+				<a href="<?php echo $facebook_url; ?>">Check Your Facebook</a><br />
+				<a href ="<?php echo $twitter_handle; ?>">Check Your Twitter</a><br />
 			</p>
 		</div>
 		<div id="footer"></div>
