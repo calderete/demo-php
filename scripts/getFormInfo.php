2385a029057e9a01d25e40969bd078153ad54f5f
Charles <codprestige10@gmail.com>
2016-03-02 16:41:41 -0500

    got some text searching and linking done

--- scripts/getFormInfo.php
+++ scripts/getFormInfo.php
@@ -3,6 +3,10 @@
 	$last_name = $_REQUEST["last_name"];
 	$email = $_REQUEST["email"];
 	$facebook_url = $_REQUEST["facebook_url"];
+	$position = strpos($facebook_url, "facebook.com");
+	if ($position === false) {
+		$facebook_url = "http://www.facebook.com/" . $facebook_url;
+	}
 	$twitter_handle = $_REQUEST["twitter_handle"];
 
 ?>
@@ -21,7 +25,7 @@
 			<p>
 				Name: <?php echo $first_name . " " . $last_name; ?><br />
 				Email: <?php echo $email; ?><br />
-				Facebook: <?php echo $facebook_url; ?><br />
+				<a href="<?php echo $facebook_url; ?>">Your Facebook</a><br />
 				Twitter: <?php echo $twitter_handle; ?><br />
 			</p>
 		</div>
